523f6280501851a901fbbd47a66fcc225530ab1b
Alexandre Tavano <AlexandreTavanoDeveloper@outlook.com>
2021-03-12 23:45:25 -0300

    Anotações e update banco

---
+++ Vidracaria_Portal/Migrations/20210313024337_NovasTabelas.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidracaria_Portal.Migrations
+{
+    public partial class NovasTabelas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataNascimento",
+                table: "AspNetUsers",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nome",
+                table: "AspNetUsers",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "ALUMINIOS",
+                columns: table => new
+                {
+                    CodigoAluminio = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    NomeAluminio = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    CorAluminio = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Marca = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    EstoqueMaximo = table.Column<int>(type: "int", nullable: false),
+                    EstoqueMinimo = table.Column<int>(type: "int", nullable: false),
+                    PrecoVenda = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    PrecoFabrica = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ALUMINIOS", x => x.CodigoAluminio);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "APROVADOS",
+                columns: table => new
+                {
+                    CodigoAprovado = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Cidade = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    Bairro = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    NumeroCasa = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    Celular = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    Documento = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_APROVADOS", x => x.CodigoAprovado);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CLIENTES",
+                columns: table => new
+                {
+                    CodigoCliente = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    NomeCliente = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    CidadeCliente = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    Bairro = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    NumeroCasa = table.Column<int>(type: "int", nullable: false),
+                    EstadoCliente = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    Celular = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    Documento = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CLIENTES", x => x.CodigoCliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CONCLUIDOS",
+                columns: table => new
+                {
+                    CodigoConcluido = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Cidade = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    Bairro = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    NumeroCasa = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    Celular = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    Documento = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CONCLUIDOS", x => x.CodigoConcluido);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FERRAGENS",
+                columns: table => new
+                {
+                    CodigoFerragem = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    NomeFerragem = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    CorFerragem = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    Marca = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    EstoqueMaximo = table.Column<int>(type: "int", nullable: false),
+                    EstoqueMinimo = table.Column<int>(type: "int", nullable: false),
+                    PrecoVenda = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    PrecoFabrica = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FERRAGENS", x => x.CodigoFerragem);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ORCAMENTOS",
+                columns: table => new
+                {
+                    CodigoOrcamento = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Cidade = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Bairro = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    NumeroCasa = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    Celular = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    Documento = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ORCAMENTOS", x => x.CodigoOrcamento);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PENDENTES",
+                columns: table => new
+                {
+                    CodigoPendentes = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Cidade = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    Bairro = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    NumeroCasa = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    Celular = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    Documento = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PENDENTES", x => x.CodigoPendentes);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VIDROS_COMUNS",
+                columns: table => new
+                {
+                    CodigoVidro = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    NomeVidro = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    CorVidro = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    Marca = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    EstoqueMaximo = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    EstoqueMinimo = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Expessura = table.Column<int>(type: "int", nullable: false),
+                    PrecoVenda = table.Column<int>(type: "int", nullable: false),
+                    PrecoFabrica = table.Column<int>(type: "int", nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VIDROS_COMUNS", x => x.CodigoVidro);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VIDROS_TEMPERADOS",
+                columns: table => new
+                {
+                    CodigoVidro = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    NomeVidro = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    CorVidro = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    Marca = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false),
+                    EstoqueMaximo = table.Column<int>(type: "int", nullable: false),
+                    EstoqueMinimo = table.Column<int>(type: "int", nullable: false),
+                    Expessura = table.Column<int>(type: "int", nullable: false),
+                    PrecoVenda = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    PrecoFabrica = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VIDROS_TEMPERADOS", x => x.CodigoVidro);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "13f0eea5-f883-4975-a780-709f0e994547", "14f34493-9f53-484c-849c-1f9c5f95a73b", "Administrador", "ADMINISTRADOR" },
+                    { "cb42e9e4-1c97-41e2-b746-f0bdadbe2d65", "702f0189-ce16-40aa-a3b5-96dc763a218f", "Cliente", "CLIENTE" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "DataNascimento", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "Nome", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "f637586f-8624-4d4f-8714-9b81953b6606", 0, "80a6f1e6-ee6c-40be-98bc-73a74b6591e3", new DateTime(2021, 3, 12, 23, 43, 36, 59, DateTimeKind.Local).AddTicks(500), "admin@vidracariaportal.com.br", true, false, null, "Admin", "ADMIN@VIDRACARIAPORTAL.COM.BR", "ADMIN@VIDRACARIAPORTAL.COM.BR", "AQAAAAEAACcQAAAAEJbhZ4XC4+xpTdxVM2xgnQIScKe6DM8yKClPHMbIP82Asu8azwexKQMiFMbBB4KpRg==", null, false, "32922880", false, "admin@vidracariaportal.com.br" },
+                    { "1f48dc88-33e6-46e5-9914-8f4bbd27884e", 0, "02aefb4e-20b3-43bb-9980-9635edef8f2c", new DateTime(2002, 8, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "tavanoalexandre@outlook.com", true, false, null, "Alexandre Tavano Cardoso", "TAVANOALEXANDRE@OUTLOOK.COM", "TAVANOALEXANDRE@OUTLOOK.COM", "AQAAAAEAACcQAAAAENio8+isGDQ4XOK+bJHxhrmypAcXs3bk389q1aexIOOzgQywh023ROr8RXM06IcYSA==", null, false, "27870469", false, "tavanoalexandre@outlook.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "13f0eea5-f883-4975-a780-709f0e994547", "f637586f-8624-4d4f-8714-9b81953b6606" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "cb42e9e4-1c97-41e2-b746-f0bdadbe2d65", "1f48dc88-33e6-46e5-9914-8f4bbd27884e" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ALUMINIOS");
+
+            migrationBuilder.DropTable(
+                name: "APROVADOS");
+
+            migrationBuilder.DropTable(
+                name: "CLIENTES");
+
+            migrationBuilder.DropTable(
+                name: "CONCLUIDOS");
+
+            migrationBuilder.DropTable(
+                name: "FERRAGENS");
+
+            migrationBuilder.DropTable(
+                name: "ORCAMENTOS");
+
+            migrationBuilder.DropTable(
+                name: "PENDENTES");
+
+            migrationBuilder.DropTable(
+                name: "VIDROS_COMUNS");
+
+            migrationBuilder.DropTable(
+                name: "VIDROS_TEMPERADOS");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "cb42e9e4-1c97-41e2-b746-f0bdadbe2d65", "1f48dc88-33e6-46e5-9914-8f4bbd27884e" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "13f0eea5-f883-4975-a780-709f0e994547", "f637586f-8624-4d4f-8714-9b81953b6606" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "13f0eea5-f883-4975-a780-709f0e994547");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cb42e9e4-1c97-41e2-b746-f0bdadbe2d65");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1f48dc88-33e6-46e5-9914-8f4bbd27884e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "f637586f-8624-4d4f-8714-9b81953b6606");
+
+            migrationBuilder.DropColumn(
+                name: "DataNascimento",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Nome",
+                table: "AspNetUsers");
+        }
+    }
+}

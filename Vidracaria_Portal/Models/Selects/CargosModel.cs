9c796d711502c23177a41e55f33674372c265f43
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-03-29 12:34:37 -0300

    Add files via upload

---
+++ Vidracaria_Portal/Models/Selects/CargosModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vidracaria_Portal.Models.Selects
+{
+    [Table("CARGOS")]
+    public class CargosModel
+    {
+        [Key]
+        public int CodigoCargo { get; set; }
+        public string NomeCargo { get; set; }
+    }
+}
